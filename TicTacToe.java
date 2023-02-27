63dca3f5994e42f93f58594dddb5b134edbe1003
s15236 <s15236@pjwstk.edu.pl>
2023-02-27 05:12:01 +0100

    Initial commit.

---
+++ TicTacToe.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+
+    static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+            System.out.println("\nLet's play tic tac toe");
+            
+            scan.close();
+        }
+
+}
